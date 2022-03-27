69bbca37f10327cbfd8fd68e8cd46f7ecdf103c4
Christ Kamanda <kamandachristo@gmail.com>
2022-03-27 16:32:45 -0400

    prework arrow-functions

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const divide = function (){
+    return 2000 / 100
+} 
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2
+}
+
+add(2,4)
+
+const square = (parameter1) => {
+    return parameter1 * parameter1
+}
+
+square(2)
